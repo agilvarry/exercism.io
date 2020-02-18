3b1e795a83e34090be4c0141805f8e3aaa8044b5
Alex Gilvarry <alex.gilvarry@gmail.com>
2020-02-18 15:26:39 -0700

    pangram

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,2 +1,13 @@
+import string
+
 def is_pangram(sentence):
-    pass
+    
+    alphabet = set(string.ascii_lowercase)
+    sentence_set = set(removeNonAscii(sentence))
+
+    if len(alphabet) > len(sentence_set) and len(sentence_set) > 0:
+        return True
+    else:
+        return False
+
+def removeNonAscii(s): return "".join(filter(lambda x: ord(x)<128, s))
