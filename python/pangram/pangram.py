482f1f192f880300d2afec2063068afbebba526f
Alex Gilvarry <alex.gilvarry@gmail.com>
2020-02-20 14:42:53 -0700

    changes

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,13 +1,12 @@
 import string
 
+def removeNonAscii(s): return filter(str.isalpha, s)
 def is_pangram(sentence):
     
     alphabet = set(string.ascii_lowercase)
-    sentence_set = set(removeNonAscii(sentence))
+    sentence_set = set(removeNonAscii(sentence.lower()))
 
-    if len(alphabet) > len(sentence_set) and len(sentence_set) > 0:
+    if len(alphabet) == len(sentence_set):
         return True
     else:
         return False
-
-def removeNonAscii(s): return "".join(filter(lambda x: ord(x)<128, s))
