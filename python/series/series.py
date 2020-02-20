67343150744ab1026b8251c3f1ae886faeae0fce
Alex Gilvarry <alex.gilvarry@gmail.com>
2020-02-20 15:51:15 -0700

    raindrops

--- python/series/series.py
+++ python/series/series.py
@@ -1,2 +1,10 @@
 def slices(series, length):
-    pass
+    if length > len(series):
+       raise ValueError("Length cannot be longer than series")
+    elif length <1:
+        raise ValueError("Length cannot be less than 1")
+    else:
+        slices = []
+        for i in range(len(series)-length+1):   
+            slices.append(series[i:i+length])
+        return slices
