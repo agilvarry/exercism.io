41db5e9db8cfbb6c9e83d2d0a6599fa2c861fb20
Alex Gilvarry <alex.gilvarry@gmail.com>
2020-02-25 08:51:59 -0700

    simplify pangram

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,12 +1,8 @@
 import string
 
 def removeNonAscii(s): return filter(str.isalpha, s)
+
 def is_pangram(sentence):
-    
     alphabet = set(string.ascii_lowercase)
     sentence_set = set(removeNonAscii(sentence.lower()))
-
-    if len(alphabet) == len(sentence_set):
-        return True
-    else:
-        return False
+    return len(alphabet) == len(sentence_set)
