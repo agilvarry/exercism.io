f8b3428978fc6f7ed9ddde437d3875d7f3d0c72c
Alex Gilvarry <alex.gilvarry@gmail.com>
2020-03-04 11:05:15 -0700

    submit matrix

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,9 +1,12 @@
 class Matrix:
     def __init__(self, matrix_string):
-        pass
+        self.matrix_string = self.make_matrix(matrix_string)
+
+    def make_matrix(self, matrix_string):
+        return [i.split(' ') for i in matrix_string.split('\n')]
 
     def row(self, index):
-        pass
+        return [int(i) for i in self.matrix_string[index-1]]
 
     def column(self, index):
-        pass
+        return [int(i[index-1]) for i in self.matrix_string]
